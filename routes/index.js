5c75f2c8e844a7d6d37f4f743e1de9ceb94c004b
Michael Claro <stayputman@gmail.com>
2020-10-11 03:06:51 -0400

    Included Nav Bar and deleted unneeded code

--- routes/index.js
+++ routes/index.js
@@ -3,32 +3,29 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home Page' });
 });
 /* GET home page. */
 //Added incase user types /home
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home Page' });
 });
-/* GET About page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
-});
-/* GET Products page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products' });
+/* GET Projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
 });
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
   res.render('index', { title: 'Services' });
 });
-/* GET About Us page. */
-router.get('/aboutus', function(req, res, next) {
-  res.render('index', { title: 'About Us' });
+/* GET About Me page. */
+router.get('/aboutme', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
 });
-/* GET Contact Us page. */
-router.get('/contactus', function(req, res, next) {
-  res.render('index', { title: 'Contact Us' });
+/* GET Contact me page. */
+router.get('/contactme', function(req, res, next) {
+  res.render('index', { title: 'Contact Me' });
 });
+
 
 module.exports = router;
