d33bc8dfd9f66ff054e0a9af1762b74d20b45c90
Michael Claro <stayputman@gmail.com>
2020-10-11 05:34:29 -0400

    Multiple .ejs files were added

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,28 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home Page' });
+  res.render('index', { title: 'Home' });
 });
 /* GET home page. */
 //Added incase user types /home
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home Page' });
+  res.render('index', { title: 'Home' });
 });
 /* GET Projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'Projects' });
+  res.render('projects', { title: 'Projects' });
 });
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('services', { title: 'Services' });
 });
 /* GET About Me page. */
 router.get('/aboutme', function(req, res, next) {
-  res.render('index', { title: 'About Me' });
+  res.render('aboutme', { title: 'About Me' });
 });
 /* GET Contact me page. */
 router.get('/contactme', function(req, res, next) {
-  res.render('index', { title: 'Contact Me' });
+  res.render('contactme', { title: 'Contact Me' });
 });
 
 
